e2ee8b1ae2bcb78223cf24523adeb8868c1d8f77
Steve de Vette <sdevette@Steves-MacBook-Pro.local>
2019-01-03 14:03:40 +0100

    Init Quasar

--- src/main.js
+++ src/main.js
@@ -4,6 +4,17 @@ import router from './router'
 import store from './store'
 import './registerServiceWorker'
 
+import './styles/quasar.styl'
+import 'quasar-framework/dist/quasar.ie.polyfills'
+import 'quasar-extras/animate'
+import 'quasar-extras/roboto-font'
+import 'quasar-extras/material-icons'
+import Quasar from 'quasar'
+
+Vue.use(Quasar, {
+  config: {}
+ })
+
 Vue.config.productionTip = false
 
 new Vue({
