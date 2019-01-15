319b29c9e306a5e5e720d405a6e148fd14f73858
Steve de Vette <sdevette@Steves-MacBook-Pro.local>
2019-01-15 16:20:32 +0100

    Start password strength checker

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -161,6 +161,26 @@ function parseCountries () {
     }
   })
 }
+
+function classifyChar (char) {
+  let code = char.charCodeAt(0);
+  if(code >= 32 && code <= 47){
+    return "symbol";
+  } else if(code >= 48 && code <= 57){
+    return "digit";
+  } else if(code >= 58 && code <= 64){
+    return "symbol";
+  } else if(code >= 65 && code <= 90){
+    return "uppercase";
+  } else if(code >= 91 && code <= 96){
+    return "symbol";
+  } else if(code >= 97 && code <= 122){
+    return "lowercase";
+  } else if(code > 122){
+    return "symbol";
+  }
+}
+
 export default {
   name: 'Register',
   data: () => ({
@@ -201,7 +221,51 @@ export default {
     loading: false,
     countries: parseCountries()
   }),
+  mounted() {
+    this.calculatePasswordStrength("'/192aX!ba§");
+  },
   methods: {
+    calculatePasswordStrength (password){
+      let string = password;
+
+      let incremented = 0;
+      let differentSymbol = 0;
+      let duplicate = 0;
+      for(let c = 0; c < string.length; c++){
+        let current = {
+          value: string[c],
+          type: classifyChar(string[c]),
+          code: string[c].charCodeAt(0)
+        }
+        console.log(c * c);
+
+        if(string[c+1]){
+          let next = {
+            value: string[c+1],
+            type: classifyChar(string[c+1]),
+            code: string[c+1].charCodeAt(0)
+          }
+          // Checks to see if the next character is just an increment of the current one
+          // This scores passwords like "1234" verry poorly, like it should
+          if(Math.abs(current.code - next.code) === 1){
+            incremented++;
+          }
+          // Checks to see is the next character is of a different type 
+          // upper/lowercase, digits or symbols. Encouraging passwords like "aX!1+HiA"
+          if(current.type !== next.type){
+            differentSymbol++;
+          }
+          // Repeating the same character is like the worst thing ever
+          // Don't do it!
+          if(current.code === next.code){
+            duplicate + 3;
+          }
+        }
+      }
+
+      let score = incremented + differentSymbol + duplicate;
+      console.log("Tested against: ", password, " with score: ", score);
+    },
     searchCountry (terms, done) {
       done(filter(terms, {field: 'value', list: parseCountries()}))
     },
